22f048586a7c66de4a51c03947430aa5735c2fa1
Rohithegade02 <rohithegade8@gmail.com>
2025-12-13 13:41:18 +0530

    feat: replace lazy video view text fallback with skeleton and safe area view

--- app/(main)/video-layout.tsx
+++ app/(main)/video-layout.tsx
@@ -1,13 +1,22 @@
 
-import { Text } from '@/components/atoms';
+import { Skeleton } from '@/components/atoms/skeleton';
 import React, { lazy, Suspense } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const LazyVideoView = lazy(() => import('@/screens/VideoViewScreen/VideoViewScreenContainer'));
 
 export default function IndexLayout() {
     return (
-        <Suspense fallback={<Text>loading Video View...</Text>}>
+        <Suspense fallback={<FallBack />}>
             <LazyVideoView />
         </Suspense>
+    )
+}
+
+const FallBack = () => {
+    return (
+        <SafeAreaView className="flex bg-white px-5 flex-row items-center gap-4">
+            <Skeleton className="h-[80vh]  mb-5" />
+        </SafeAreaView>
     )
 }
